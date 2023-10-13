c7f717c17c9a30d8e74fdec73c0ccaad31020fec
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-10-13 22:34:50 +0530

    Create a.py

---
+++ a.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf AMMusic && git clone https://github.com/AbhiModszYT/AMMusic && cd AMMusic && python3 -m AM")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
