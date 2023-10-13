29fb21f485b00c2a6229f364b13cc4ea68d0072a
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-10-13 20:38:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/8d3944528638ff176d115.jpg"
+YOUTUBE_IMG_URL = None
 
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 
